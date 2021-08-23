812ef4840ac02d9be90185e38e5c63b513370033
VBenny42 <vinesh.benny@mail.utoronto.ca>
2021-08-23 13:17:14 -0400

    Finished q6 of apr17

--- B09April17/test.c
+++ B09April17/test.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
-    int sad = 0;
-    int *hj = &sad;
-    printf("%d\n", *(hj));
+	int var = 1;
+	int status;
+	
+	int r = fork();
+	if(r == 0) {
+		var++;
+		r = fork();
+		if(r == 0) { // process X
+			var++;
+			exit(var);
+		} else { // process Y
+			if(wait(&status) != -1) {
+				if(WIFEXITED(status)) {
+					printf("A %d ", WEXITSTATUS(status));
+				}
+			}
+			var += 2; 
+		}
+	} else { // process Z
+		printf("W %d ", var);
+		if(wait(&status) != -1) {
+			if(WIFEXITED(status)) {
+				printf("B %d ", WEXITSTATUS(status));
+			} 
+		}
+	}
+	printf("C %d ", var);
+	return 0;
 }
